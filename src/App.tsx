3a79bbdaacdbdb1d1afd79f079d9ca3dd3e31f32
gjvishnu <gjvishnu24@gmail.com>
2024-11-17 23:36:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+ 
+ 
+function App() {
+ 
+  return (
+     <>
+      <div className="bg-blue-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">Tailwind is Working!</h1>
+    </div>
+     </>
+  )
+}
+
+export default App
